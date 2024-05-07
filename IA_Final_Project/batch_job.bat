e6c838f8622d3db1687d257437572660258cfaf3
mimam786 <mudassirimam02@gmail.com>
2024-05-07 03:11:19 -0400

    added more python scripts and other relevant document

--- IA_Final_Project/batch_job.bat
+++ IA_Final_Project/batch_job.bat
@@ -14,6 +14,8 @@ echo Conversion completed.
 REM Paths to your Python scripts
 set SCRIPT1=Python_Scripts\Air_Script.py
 set SCRIPT2=Python_Scripts\Water_Script.py
+set SCRIPT3=Python_Scripts\Create_OLTP_Tables.py
+set SCRIPT4=Python_Scripts\Glue_Workflow.py
 
 REM Run Script 1 and wait for it to finish
 echo Running Script 1...
@@ -32,6 +34,27 @@ if %ERRORLEVEL% neq 0 (
     
 )
 echo Script 2 completed successfully.
+
+REM Run Script 3 and wait for it to finish
+echo Running Script 3...
+python %SCRIPT3%
+if %ERRORLEVEL% neq 0 (
+    echo Error occurred while running Script 3. Exiting.
+    
+)
+echo Script 3 completed successfully.
+
+REM Add a delay of 5 seconds without displaying the countdown
+timeout /t 20 /nobreak > nul
+
+REM Run Script 4 and wait for it to finish
+echo Running Script 4...
+python %SCRIPT4%
+if %ERRORLEVEL% neq 0 (
+    echo Error occurred while running Script 4. Exiting.
+    
+)
+echo Script 4 completed successfully.
 
 REM Pause to keep the command window open
 pause
